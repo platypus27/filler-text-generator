f1c4912794c45735890e9cdc8f3432fe2bda6686
Ao Yu <84365804+platypus27@users.noreply.github.com>
2024-06-09 16:31:39 +0800

    update

--- index.py
+++ index.py
@@ -1,18 +1,43 @@
-import random
+"""
+Random filler text generator for testing purposes and generating mock data
 
-def getWords():
-    wordlist = []  
-    words = open("words.txt", "r").read().splitlines()
-    return words
+Usage:
+    python index.py <wordcount>
     
-def main():
-    wordplist = []
-    words = getWords()
-    wordcount = input("text length: ")
-    for i in range(int(wordcount)):
-        wordplist.append(random.choice(words))
+Author:
+    aoyu (ngaoyu27@gmail.com)
+    
+Version:
+    1.0
+"""
+
+#imports
+import random
+import sys
+
+
+# read file and store as global var
+WORDS = open("words.txt", "r").read().splitlines()
+
+
+def generate_words(wordcount: int) -> str:
+    """Generate a random paragraph of words
+
+    Args:
+        wordcount (int): _description_
+
+    Returns:
+        str: _description_
+    """
+    wordplist = [random.choice(WORDS) for _ in range(wordcount)]
     wordpara = " ".join(wordplist)
-    print(wordpara)
-    print("\ntext generated")
-    
-main()
+    return wordpara
+
+
+def main():
+    wordcount = int(sys.argv[1])
+    print(generate_words(wordcount))
+
+
+if __name__ == "__main__":
+    main()
